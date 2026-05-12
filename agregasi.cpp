a8efe93fc9c19a4a82c6974acb60e66a4f45e065
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-05-12 19:17:03 +0700

    agregasi

--- agregasi.cpp
+++ agregasi.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-#ifndef ANAK_H
-#define ANAK_H
+#include "anak.h"
+#include "ibu.h"
 
-// membuat class anak di file header anak.h
-class anak
-{
-public:
-    // deklarasi member variabel pada class anak
-    string nama;
+int main()
+{ // agregasi
+    ibu *varIbu = new ibu("dini");
+    ibu *varIbu2 = new ibu("novi");
+    anak *varAnak1 = new anak("tono");
+    anak *varAnak2 = new anak("rini");
+    anak *varAnak3 = new anak("dewi");
 
-    // membuat constructor dan destructor pada class anak
-    anak(string pNama) : nama(pNama)
-    {
-        cout << "Anak \"" << nama << "\" ada \n";
-    }
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
 
-    ~anak()
-    {
-        cout << "Anak \"" << nama << "\" tidak ada\n";
-    }
-};
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
 
-#endif
+    delete varIbu;
+    delete varIbu2;
+    delete varanak1;
+    delete varanak2;
+    delete varanak3;
+
+    system("pause");
+    return 0;
+}
